3245729104042c5b99880cf7501efd361a15113e
Oliver-Vandevelde <oliver.vandevelde7@gmail.com>
2019-04-17 10:21:15 +0200

    afichage-ajax

---
+++ script.js
@@ -0,0 +1,28 @@
+//Create the XHR Object
+let xhr = new XMLHttpRequest;
+//Call the open function, GET-type of request, url, true-asynchronous
+xhr.open('GET', 'https://thatsthespir.it/api', true)
+//call the onload 
+xhr.onload = function() 
+    {
+        //check if the status is 200(means everything is okay)
+        if (xhr.status === 200) 
+            {
+
+                //return server response as an object with JSON.parse
+                console.log(JSON.parse(xhr.responseText));
+                let reponse = (JSON.parse(xhr.responseText));
+                document.getElementById("quote").innerHTML = reponse["quote"];
+                document.getElementById("photo").src = reponse["photo"];
+                document.getElementById("author").innerHTML = reponse["author"];
+                
+    }
+            }
+//call send
+xhr.send();
+//Common Types of HTTP Statuses
+// 200: OK
+// 404: ERROR
+// 403: FORBIDDEN
+
+
